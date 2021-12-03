bf3200404a2a1970574f21ea9a21d70da9d0acb4
taichi <taicloud0803@icloud.com>
2021-12-03 21:39:32 +0900

    commit

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'RxSwiftPractice' do
+
+  pod 'RxSwift'
+  pod 'RxCocoa'
+
+  # Pods for RxSwiftPractice
+
+  target 'RxSwiftPracticeTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'RxSwiftPracticeUITests' do
+    # Pods for testing
+  end
+
+end
--- RxSwiftPractice/ViewController.swift
+++ RxSwiftPractice/ViewController.swift
@@ -6,13 +6,88 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+
+
+struct Product {
+    let imageUrl:String
+    let title:String
+}
+
+struct ProductViewModel {
+    var items = PublishSubject<[Product]>()
+    
+    func fetchItems(){
+        let products = [
+        Product(imageUrl: "house", title: "Home"),
+        Product(imageUrl: "gear", title: "Setting"),
+        Product(imageUrl: "person.circle", title: "Profile"),
+        Product(imageUrl: "airplane", title: "Fights"),
+        Product(imageUrl: "bell", title: "Activity")
+        ]
+        
+        items.onNext(products)
+        items.onCompleted()
+    }
+    
+}
+
+
+
+
 
 class ViewController: UIViewController {
-
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    private var viewModel = ProductViewModel()
+    
+    private var bag = DisposeBag()
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(tableView)
+        tableView.frame = view.bounds
+        bindTableData()
+        
+    
     }
+    
+    
+    
+    
+    
+    func bindTableData() {
+        
+        viewModel.items.bind(to: tableView.rx.items(cellIdentifier: "cell", cellType: UITableViewCell.self)) { row, model, cell in
+            cell.textLabel?.text = model.title
+            cell.imageView?.image = UIImage(systemName: model.imageUrl)
+        }.disposed(by: bag)
+        
+        tableView.rx.modelSelected(Product.self).bind { product in
+            print(product.title)
+        }.disposed(by: bag)
+        
+        viewModel.fetchItems()
+    }
+    
+    
+    
+    
+    
+    
+    
+    
 
 
 }
